1a759605ff408e2e5a2d39df8854d24fb9c1824f
mta630 <mta630@ddubb.net>
2024-10-05 02:26:00 -0600

    added spiffo pics and started coding a singular spiffo component

--- src/components/spiffo.tsx
+++ src/components/spiffo.tsx
@@ -1,20 +1,25 @@
-import { cn } from "@/lib/utils";
+import Image from "next/image";
 import * as React from "react";
 
-export interface SpiffoProps extends React.HTMLAttributes<HTMLDivElement> {
-  className?: string;
-}
+type SpiffoProps = React.HTMLAttributes<HTMLImageElement>;
 
-const Spiffo = React.forwardRef<HTMLDivElement, SpiffoProps>(
-  ({ className, ...props }, ref) => (
-    <div
-      ref={ref}
-      className={cn("rounded-lg border shadow-sm", className)}
-      {...props}
-    />
-  )
-);
-
-Spiffo.displayName = "Spiffo";
-
-export { Spiffo };
+export const Spiffo = {
+  404: (props: SpiffoProps) => {
+    return (
+      <Image
+        src="/spiffo/404.png"
+        alt="Spiffo the red raccoon holding a 404 sign"
+        {...props}
+      />
+    );
+  },
+  surfer: (props: SpiffoProps) => {
+    return (
+      <Image
+        src="/spiffo/surfer.png"
+        alt="Spiffo the red raccoon surfing"
+        {...props}
+      />
+    );
+  },
+};
